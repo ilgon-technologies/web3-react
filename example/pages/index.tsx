a0b68514bcb7353a0b1047aec478c3bc09d2b669
Koppany Papp <hhpkoop@gmail.com>
2021-06-15 17:49:47 +0200

    remove not needed connectors and upgrade deps

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -5,25 +5,15 @@ import {
   UserRejectedRequestError as UserRejectedRequestErrorInjected
 } from '@web3-react/injected-connector'
 import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector'
-import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from '@web3-react/frame-connector'
 import { Web3Provider } from '@ethersproject/providers'
 import { formatEther } from '@ethersproject/units'
 
 import { useEagerConnect, useInactiveListener } from '../hooks'
 import {
   injected,
-  network,
   walletconnect,
-  walletlink,
   ledger,
   trezor,
-  lattice,
-  frame,
-  authereum,
-  fortmatic,
-  magic,
-  portis,
-  torus
 } from '../connectors'
 import { Spinner } from '../components/Spinner'
 import { AbstractConnector } from '@web3-react/abstract-connector'
@@ -31,34 +21,16 @@ import { PrivateKeyConnector } from "../private-key-connector";
 
 enum ConnectorNames {
   Injected = 'Injected',
-  Network = 'Network',
   WalletConnect = 'WalletConnect',
-  WalletLink = 'WalletLink',
   Ledger = 'Ledger',
   Trezor = 'Trezor',
-  Lattice = 'Lattice',
-  Frame = 'Frame',
-  Authereum = 'Authereum',
-  Fortmatic = 'Fortmatic',
-  Magic = 'Magic',
-  Portis = 'Portis',
-  Torus = 'Torus'
 }
 
 const connectorsByName: { [connectorName in ConnectorNames]: any } = {
   [ConnectorNames.Injected]: injected,
-  [ConnectorNames.Network]: network,
   [ConnectorNames.WalletConnect]: walletconnect,
-  [ConnectorNames.WalletLink]: walletlink,
   [ConnectorNames.Ledger]: ledger,
   [ConnectorNames.Trezor]: trezor,
-  [ConnectorNames.Lattice]: lattice,
-  [ConnectorNames.Frame]: frame,
-  [ConnectorNames.Authereum]: authereum,
-  [ConnectorNames.Fortmatic]: fortmatic,
-  [ConnectorNames.Magic]: magic,
-  [ConnectorNames.Portis]: portis,
-  [ConnectorNames.Torus]: torus
 }
 
 function getErrorMessage(error: Error) {
@@ -68,8 +40,7 @@ function getErrorMessage(error: Error) {
     return "You're connected to an unsupported network."
   } else if (
     error instanceof UserRejectedRequestErrorInjected ||
-    error instanceof UserRejectedRequestErrorWalletConnect ||
-    error instanceof UserRejectedRequestErrorFrame
+    error instanceof UserRejectedRequestErrorWalletConnect
   ) {
     return 'Please authorize this website to access your Ethereum account.'
   } else {
@@ -238,11 +209,6 @@ function Header() {
 function KillSessionButton({ connector }: { connector: AbstractConnector }) {
   const killableConnectors: AbstractConnector[] = [
     walletconnect,
-    walletlink,
-    fortmatic,
-    magic,
-    portis,
-    torus,
   ];
   if (killableConnectors.includes(connector)) {
     const name = Object.entries(connectorsByName).find(
@@ -263,46 +229,6 @@ function KillSessionButton({ connector }: { connector: AbstractConnector }) {
   } else {
     return null;
   }
-}
-
-function switchNetworkButton(connector: AbstractConnector, chainId: number) {
-  return <>
-    {!!(
-      connector === connectorsByName[ConnectorNames.Network] && chainId
-    ) && (
-      <button
-        style={{
-          height: '3rem',
-          borderRadius: '1rem',
-          cursor: 'pointer'
-        }}
-        onClick={() => {
-          ;(
-            connector as any
-          ).changeChainId(chainId === 1 ? 4 : 1)
-        }}
-      >
-        Switch Networks
-      </button>
-    )}
-    {connector === connectorsByName[ConnectorNames.Portis] &&
-    chainId !== undefined && (
-      <button
-        style={{
-          height: '3rem',
-          borderRadius: '1rem',
-          cursor: 'pointer'
-        }}
-        onClick={() => {
-          ;(
-            connector as any
-          ).changeNetwork(chainId === 1 ? 100 : 1)
-        }}
-      >
-        Switch Networks
-      </button>
-    )}
-  </>;
 }
 
 function signMessageButton(library: Web3Provider, account: string) {
@@ -476,8 +402,7 @@ function ConnectPrivateKeyButton(
 }
 
 function App() {
-  const { connector, library, chainId, account, activate, deactivate, active, error } = useWeb3React<Web3Provider>()
-
+  const { connector, library, account, activate, deactivate, active, error } = useWeb3React<Web3Provider>()
   // handle logic to recognize the connector currently being activated
   const [activatingConnector, setActivatingConnector] = React.useState<any>()
   React.useEffect(() => {
@@ -562,7 +487,6 @@ function App() {
         }}
       >
         {signMessageButton(library, account)}
-        {switchNetworkButton(connector, chainId)}
         <KillSessionButton connector={connector}/>
       </div>
     </>
