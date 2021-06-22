9c7c0e8acf2a90f85c30cc7248f7a572b33e8ac3
Koppany Papp <hhpkoop@gmail.com>
2021-06-22 19:59:48 +0200

    remove redundant casts

--- example/connectors.ts
+++ example/connectors.ts
@@ -50,8 +50,8 @@ export async function getWallet(jsonfile: Record<string, unknown>, password: str
 
 const POLLING_INTERVAL = 12000
 const RPC_URLS: { [chainId: number]: string } = {
-  1: process.env.RPC_URL_1 as string,
-  4: process.env.RPC_URL_4 as string
+  1: process.env.RPC_URL_1,
+  4: process.env.RPC_URL_4
 }
 
 export const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
