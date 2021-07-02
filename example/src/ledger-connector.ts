a7bf430f5975635af91450ac97f8296e19d2b788
Koppany Papp <hhpkoop@gmail.com>
2021-07-02 19:28:10 +0200

    refactor

--- example/src/ledger-connector.ts
+++ example/src/ledger-connector.ts
@@ -1,4 +1,3 @@
-import { ConnectorUpdate } from '@web3-react/types'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 import Web3ProviderEngine from 'web3-provider-engine'
 import { LedgerSubprovider } from '@0x/subproviders/lib/src/subproviders/ledger' // https://github.com/0xProject/0x-monorepo/issues/1400
@@ -10,56 +9,50 @@ import { LedgerEthereumClient } from '@0x/subproviders'
 interface LedgerConnectorArguments {
   chainId: number
   url: string
-  pollingInterval?: number
-  requestTimeoutMs?: number
-  accountFetchingConfigs?: any
-  baseDerivationPath?: string
 }
 
 const ledgerEthereumClientFactoryAsync = async () => new Eth(await TransportWebUSB.create()) as LedgerEthereumClient
 
 export class LedgerConnector extends AbstractConnector {
   private readonly chainId: number
-  private readonly url: string
-
-  private provider?: Web3ProviderEngine
+  private readonly provider: Web3ProviderEngine
+  private readonly subprovider: LedgerSubprovider
 
   constructor({ chainId, url }: LedgerConnectorArguments) {
     super({ supportedChainIds: [chainId] })
     this.chainId = chainId
-    this.url = url
+    const [provider, subprovider] = (() => {
+      const _provider = new Web3ProviderEngine()
+      const _subprovider = new LedgerSubprovider({
+        networkId: this.chainId,
+        ledgerEthereumClientFactoryAsync,
+      })
+      _provider.addProvider(_subprovider)
+      _provider.addProvider(new RPCSubprovider(url))
+      return [_provider, _subprovider] as const
+    })()
+    this.provider = provider
+    this.subprovider = subprovider
   }
-  public async activate(): Promise<ConnectorUpdate> {
-    if (!this.provider) {
-      const engine = new Web3ProviderEngine()
-      engine.addProvider(
-        new LedgerSubprovider({
-          networkId: this.chainId,
-          ledgerEthereumClientFactoryAsync,
-        })
-      )
-      engine.addProvider(new RPCSubprovider(this.url))
-      this.provider = engine
-    }
 
+  public async activate() {
     this.provider.start()
-
     return { provider: this.provider, chainId: this.chainId }
   }
 
-  public async getProvider(): Promise<Web3ProviderEngine> {
-    return this.provider!
+  public async getProvider() {
+    return this.provider
   }
 
-  public async getChainId(): Promise<number> {
+  public async getChainId() {
     return this.chainId
   }
 
-  public async getAccount(): Promise<null> {
-    return (this.provider as any)._providers[0].getAccountsAsync(1).then((accounts: string[]): string => accounts[0])
+  public async getAccount() {
+    return this.subprovider.getAccountsAsync(1).then(([account]) => account)
   }
 
   public deactivate() {
-    this.provider!.stop()
+    this.provider.stop()
   }
 }
